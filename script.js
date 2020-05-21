d4d3ef7e23ef45be8e559ca810843fcb285505d9
lewismcruz <lewis.cruz@gmail.com>
2020-05-21 13:39:19 -0500

    Updating Lewis branch for merge.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 
       // This pulls ALL drinks from the CocktailDB database.
 
+
+      
+
       // Original API key substituted into var queryURL was var APIKey = "1" --- key now hardcoded into URL
       var arrAlphaLetters = new Array ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
       //console.log(arrAlphaLetters);
@@ -27,8 +30,29 @@
                 var drinkName = response.drinks[0].strDrink[i];
                 console.log(drinkName);
 
-          }
+            }
                       
-      });  
+        });  
+         
 
-    } 
+        /*
+        function searchResultdrinkName (drinkName) {
+            new.document(drinkName);
+            var $h1 = $("<h1>");
+            var $p = $("<p>");
+
+            $(h1).append(document,drinkName);
+            $(p).appendChild($p);
+
+
+        }
+        // At button click of a letter, run the function to pull the drink properties matching the first
+        // letter of the name of the drink. But we do not want it to execute until letters are clicked.
+
+        // We need an element tag selection method and to create a click event. 
+ 
+        */
+
+
+      } 
+
