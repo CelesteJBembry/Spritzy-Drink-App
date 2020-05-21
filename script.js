fc08e7defcdd28320f0dfb5ac64d011c6ed2d423
lewismcruz <lewis.cruz@gmail.com>
2020-05-21 15:29:56 -0500

    Created letter alphabet bar for drinkpage.html

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Let's set the letters on screen so that users can select the first letter of their drink.
         
-
+ console.log("loaded")
       // This pulls ALL drinks from the CocktailDB database.
 
+
+      /*
       // Original API key substituted into var queryURL was var APIKey = "1" --- key now hardcoded into URL
       var arrAlphaLetters = new Array ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
       //console.log(arrAlphaLetters);
@@ -32,3 +34,68 @@
       });  
 
     } 
+ */
+// code to recycle
+/*
+ var randomDrink = $("<h3>").text(response.drinks[0].strDrink);
+        var imageDrink = $("<img>").attr("src", response.drinks[0].strDrinkThumb);
+        console.log(randomDrink)
+        $(".random").html(randomDrink);
+        $(".random").append(imageDrink);
+        $(".random").append("<h5>Click for Recipe!</h5>");
+        $(".random").append("<h5>Click for Random Drink!</h5>");
+
+
+
+*/
+
+// Global var
+var arrAlphaLetters = new Array ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+
+
+function letterGenerate () {
+
+
+  for (var i = 0; i < arrAlphaLetters.length; i++){
+  //console.log(arrAlphaLetters[i]);
+
+  // each time the for loop runs we are going to use jquery to craete a new div
+  // stored in element letterEl 
+
+
+  var newSpanForLetters = $("<span>");
+  newSpanForLetters.text(arrAlphaLetters[i].toUpperCase());
+  newSpanForLetters.css("padding", "0.8rem");
+  $(".lettersDiv").append(newSpanForLetters);
+  
+  // to that div we need to update the text content to be equal to where we are in the array
+  // .arrAlphaLetters[i]
+
+  // then we are going to append our html element letterEl to letters Div class
+
+
+  // in between the menu and the cards are at line 54 of drinkPage.html
+  // Can we use a function() { }  called when the page loads that will automatically 
+  // do this?
+
+  }
+
+}
+
+$(document).ready(function(){
+  letterGenerate();
+
+
+
+
+
+
+
+
+
+}) 
+
+
+
+
+
